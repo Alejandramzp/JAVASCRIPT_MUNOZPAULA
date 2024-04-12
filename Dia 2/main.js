1f527e7c39ac7eda67c0da29f10a2e9b93f0e05b
Paula <alejandrapmp17@gmail.com>
2024-04-12 09:03:58 -0500

    feat: :sparkles: Menu de consola
    se cambio el nombre de los archivos

---
+++ Dia 2/main.js
@@ -0,0 +1,20 @@
+var rutas =[{
+    "Ruta 1":"Ruta NodeJS",
+    "Ruta 2":"Ruta Java",
+    "Ruta 3":"Ruta NetCore"
+
+}]
+var nombres
+
+let opcion;
+opcion = prompt("MENU \n 1. Camper\n 2. Trainer\n 3.Coordinacion\n Introduce el numero de la opcion a elegir");
+console.log("MENU \n 1. Camper\n 2. Trainer\n 3.Coordinacion\n Introduce el numero de la opcion a elegir");
+if (opcion==1){
+    console.log("holi");
+}
+else if (opcion==2){
+    console.log("hola");
+}
+else if(opcion==3){
+    console.log("ey");
+}
