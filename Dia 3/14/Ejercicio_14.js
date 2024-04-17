c6b40d2e75368aaf8ef3bbe3915910b8cefd8a0f
Paula <alejandrapmp17@gmail.com>
2024-04-17 16:25:44 -0500

    feat: :wrench: Ejercicio 14 del Dia 3
    Se corrigieron los ciclos de la funcion

--- Dia 3/14/Ejercicio_14.js
+++ Dia 3/14/Ejercicio_14.js
@@ -6,22 +6,25 @@
 strs = ["flower","flow","flight"]
 
 var longestCommonPrefix = function(strs) {
-    
-    if (strs.length == 0){ //se busca el tamaño del arreglo, si es 0 se retorna vacio
-        return "";
+ 
+    if (strs.length == 0) { //se busca el tamaño del arreglo, si es 0 se retorna vacio
+        return ""; 
     }
 
-    let comparador = strs[0]; //se toma el primer elemento para compararlo 
-    
-    for (let i =1; i < strs.length; i++){ //iterador hasta el total del array
-        while (strs[i].indexOf(comparador) !== 0) {
-            comparador = comparador.slice(0, comparador.length - 1);
-            if (comparador === "") {
-                return "";
+    let prefijo = strs[0]; //se toma el primer elemento del array para compararlo 
+
+    for (let p = 0; p < prefijo.length; p++) { //iterador sobre las letras del primer elemento del array
+
+        for (let s = 1; s < strs.length; s++) { // iterador sobre el tamaño del array, un paso
+
+            if (prefijo[p] !== strs[s][p]) {//
+                // Si se encuenta un caracter diferente, se retorna el prefijo actual hasta ese punto
+                return prefijo.substring(0, p);
             }
         }
     }
-    return comparador;
+
+    return prefijo; 
 };
 
 console.log(longestCommonPrefix(strs));
