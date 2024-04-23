089dd9603caba555a11e1b82f937ee71f6551ad1
Paula <alejandrapmp17@gmail.com>
2024-04-23 08:06:23 -0500

    feat: :sparkles: Creacion de la carpeta Dia 8
    Se ingreso los datos del API de Start Wars

---
+++ Dia 8/script.js
@@ -0,0 +1,37 @@
+function fetchStarWars(){
+    let xhr = new XMLHttpRequest();
+    let url = `https://swapi.py4e.com/api/people/1`;
+    xhr.open('Get',url,true);
+    xhr.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200){
+            let response = JSON.parse(this.responseText);
+            console.log(response);
+            displayPeople(response);
+        } else if(this.readyState == 4){
+            console.log("Error:",this.statusText);
+        }
+    };
+    xhr.send();
+}
+
+function displayPeople(data){
+
+    let peopleInfo = document.getElementById("peopleStartWarsInfo");
+    if (data.response === "error"){
+        peopleInfo.innerHTML = `<p>Error: ${data.error}</p>`
+    }else{
+        peopleInfo.innerHTML = `
+        <h1>People</h1>
+        <p>Name: ${data.name}</p>
+        <p>Height: ${data.height}</p>
+        <p>Mass: ${data.mass}</p>
+        <p>Hair color: ${data.hair_color}</p>
+        <p>Skin color: ${data.skin_color}</p>
+        <p>Eye color: ${data.eye_color}</p>
+        <p>Birth year: ${data.birth_year}</p>
+        <p>Gender: ${data.gender}</p>
+       
+        `
+    }
+}
+
