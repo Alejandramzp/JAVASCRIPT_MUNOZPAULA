d52b7d348d481128dc86af9a6ab140875310d56a
Paula <alejandrapmp17@gmail.com>
2024-04-29 00:29:13 -0500

    feat: :sparkles: Creacion de Pokedex en el Dia 11

---
+++ Dia 11/js/script.js
@@ -0,0 +1,55 @@
+let pokemonID = document.getElementById("pokemonId").value;
+function fetchPokemon(pokemonID){
+    let xhr = new XMLHttpRequest();
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`;
+    xhr.open('Get',url,true);
+    xhr.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200){
+            let response = JSON.parse(this.responseText);
+            console.log(response);
+            displayPokemon(response);
+        } else if(this.readyState == 4){
+            console.log("Error:",this.statusText);
+        }
+    };
+    xhr.send();
+}
+
+document.getElementById("pokemonId").addEventListener("keyup", function(event) {
+    
+    if (event.key === "Enter") { 
+      event.preventDefault();
+      let pokemonId = document.getElementById("pokemonId").value;
+      fetchPokemon(pokemonId);
+    }
+});
+function displayPokemon(data){
+
+    let pokemonInfo = document.getElementById("pokemon");
+    if (data.response === "error"){
+        pokemonInfo.innerHTML = `<p>Error: ${data.error}</p>`
+    }else{
+        pokemonInfo.innerHTML = `
+        <p>${data.id} - ${data.name}</p>
+        `;
+    }
+
+    let pokemonGif = document.getElementById("pokeGif");
+    if (data.response === "error"){
+        pokemonGif.innerHTML = `<img class="pGif" src ="storage/llave.png">`
+    }else{
+        pokemonGif.innerHTML = `
+        <img class="pGif" src ="${data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]}">
+        `;
+    }
+
+}
+
+document.getElementById("next").addEventListener("click", function() {
+    fetchPokemon(pokemonID);
+    pokemonID++; 
+});
+document.getElementById("prev").addEventListener("click", function() {
+    fetchPokemon(pokemonID);
+    pokemonID--; 
+});
