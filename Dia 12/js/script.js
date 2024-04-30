f3a272df988992c92f174ffc594259a68e0ea459
Paula <alejandrapmp17@gmail.com>
2024-04-30 16:47:36 -0500

    feat: :sparkles: Se mezclaron las cartas

--- Dia 12/js/script.js
+++ Dia 12/js/script.js
@@ -13,12 +13,19 @@ function fetchCards(){
                 if (this.readyState === 4 && this.status === 200){
                     let response = JSON.parse(this.responseText);
                     console.log(response);
+                    let cardsOriginal = response.cards;
                     let cardsDupli = JSON.parse(JSON.stringify(response.cards)); //JSON.stringify(response.cards): convierte el array de cartas(response.cards) en una cadena JSON
                     //JSON.parse(): convierte la cadena JSON en un array de js(nueva instancia del array con sus elementos independiente del array principal)---  let cardsDupli: contiene la copia exacta del array completamente independiente
-                    response.cards = response.cards.concat(cardsDupli); //se concatena el array(cardsDupli) con el array principal(response.cards) esto crea un nuevo array que contiene todos los elementos (principales y los duplicados) y los añade al array principal
+                    let finalCards = cardsOriginal.concat(cardsDupli); //se concatena el array(cardsDupli) con el array principal(response.cards) esto crea un nuevo array que contiene todos los elementos (principales y los duplicados) y los añade al array principal
+                    for (let i = finalCards.length-1; i > 0 ; i--) {//comienza desde el ultimo elemento en el array(longitud-1) hasta 0 y se va restando de uno en uno 
+                        const j = Math.floor(Math.random()*(i+1));//se genera un numero aleatorio en j entre 0 e i,
+                        if (i !== j) {
+                        [finalCards[i],finalCards[j]]= [finalCards[j],finalCards[i]];//intercambiando posiciones i y j del arreglo y se asigna en un nuevo arreglo en las posiciones i y j.
+                        }
+                    }
                     containerCard.innerHTML = '';
-                    for (let i = 0; i < response.cards.length; i++) {
-                        displayCards(response.cards[i]); //se da la ruta para que ingrese a las caracteristicas de la carta 
+                    for (let i = 0; i < finalCards.length; i++) {
+                        displayCards(finalCards[i]); //se da la ruta para que ingrese a las caracteristicas de la carta 
                     }
                 } else if(this.readyState == 4){
                     console.log("Error:",this.statusText);
