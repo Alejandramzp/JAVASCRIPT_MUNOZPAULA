f991cb69069454c6f802725a47089a9cec2665de
Paula <alejandrapmp17@gmail.com>
2024-04-30 07:37:57 -0500

    feat: :sparkles: Mostré la carta en el tablero

--- Dia 12/js/script.js
+++ Dia 12/js/script.js
@@ -1,20 +1,19 @@
 function fetchCards(){
     let xhr = new XMLHttpRequest();
-    let url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
+    let url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`; //Barajar las cartas definiendo la cantidad de cartas a usar "count=1" 
     xhr.open('Get',url,true);
     xhr.onreadystatechange = function(){
         if (this.readyState === 4 && this.status === 200){
             let response = JSON.parse(this.responseText);
             console.log(response);
-            displayCards(response);
             let xhr = new XMLHttpRequest();
-            let url = `https://deckofcardsapi.com/api/deck/${response.deck_id}/draw/?count=1`;
+            let url = `https://deckofcardsapi.com/api/deck/${response.deck_id}/draw/?count=1`; //se entra con el ID para que muestre las cartas 
             xhr.open('Get',url,true);
             xhr.onreadystatechange = function(){
                 if (this.readyState === 4 && this.status === 200){
                     let response = JSON.parse(this.responseText);
                     console.log(response);
-                    displayCards(response);
+                   displayCards(response.cards[0]);  // 
                 } else if(this.readyState == 4){
                     console.log("Error:",this.statusText);
                 }
@@ -27,16 +26,11 @@ function fetchCards(){
     xhr.send();
 }
 
-function displayCards(data){
-    let imageCard = document.getElementById("imgCard");
-    console.log(data.cards);
-    cartasA = data.cards;
-    cartasA.forEach(cartas => {
-        console.log(cartas)
-        if (data.response === "error"){
-            imageCard.innerHTML = `<p>Error:</p>`
-        }else{
-            imageCard.innerHTML = `<img src="${cartas.images.svg}">`;
-        }
-    });
+function displayCards(card){
+    let containerCard= document.getElementById("containerCard");
+    let CardElement = document.createElement("img");
+    CardElement.src = card.image;
+    containerCard.innerHTML = '';
+    containerCard.appendChild(CardElement);
 }
+document.getElementById("buttonFetch").addEventListener("click", fetchCards);
