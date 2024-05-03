8d6081412641407db7baf38e91913a853b6894c3
Paula <alejandrapmp17@gmail.com>
2024-05-03 09:42:37 -0500

    feat: :sparkles: funcion de guardar heroe

--- Dia 13/script.js
+++ Dia 13/script.js
@@ -1,3 +1,28 @@
-a = 10;
-b = 15;
-console.log(a+b);
+const json = `{
+        "name": "Batman",
+        "nombreActor": "pepe",
+        "edadActor": "35",
+        "ubicacion": "cra 15",
+        "poster": "lalala",
+        "fecha": "12/12/2015",
+        "productora": "comics"   
+}`;
+console.log(json);
+const heroe = JSON.parse(json);
+console.log(typeof heroe);
+
+function guardar(event) {
+    let nombre = document.getElementById("nombrePer").value;
+    let nombreActor = document.getElementById("nombreAct").value;
+    let edadActor = document.getElementById("edad").value;
+    let ubicacion = document.getElementById("ubicacion").value;
+    let poster = document.getElementById("poster").value;
+    let fecha = document.getElementById("fecha").value;
+    let productora = document.getElementById("productora").value;
+    let pregunta = JSON.stringify({nombre, nombreActor, edadActor, ubicacion,poster,fecha, productora});
+    console.log(pregunta);
+    heroe.push(pregunta);
+    console.log(heroe);
+}    
+
+document.getElementById("guardar").addEventListener('click', guardar);
