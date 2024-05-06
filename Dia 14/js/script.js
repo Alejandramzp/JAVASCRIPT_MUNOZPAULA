1ce4f307217d56e2e91d5b6193dec57486427d9b
Paula <alejandrapmp17@gmail.com>
2024-05-06 09:06:17 -0500

    feat: :sparkles: Funcion de guardar Heroes

--- Dia 14/js/script.js
+++ Dia 14/js/script.js
@@ -1,28 +1,26 @@
-const json = `{
-    "name": "Batman",
-    "nombreActor": "pepe",
-    "edadActor": "35",
-    "ubicacion": "cra 15",
-    "poster": "lalala",
-    "fecha": "12/12/2015",
-    "productora": "comics"   
-}`;
-console.log(json);
-const heroe = JSON.parse(json);
-console.log(typeof heroe);
 
-function guardar(event) {
-let nombre = document.getElementById("nombrePer").value;
-let nombreActor = document.getElementById("nombreAct").value;
-let edadActor = document.getElementById("edad").value;
-let ubicacion = document.getElementById("ubicacion").value;
-let poster = document.getElementById("poster").value;
-let fecha = document.getElementById("fecha").value;
-let productora = document.getElementById("productora").value;
-let pregunta = JSON.stringify({nombre, nombreActor, edadActor, ubicacion,poster,fecha, productora});
-console.log(pregunta);
-heroe.push(pregunta);
-console.log(heroe);
+function guardar() {
+    let nombre = document.getElementById("nombrePer").value; // Obtener los valores del formulario
+    let nombreActor = document.getElementById("nombreAct").value;
+    let edad = document.getElementById("edad").value;
+    let ubicacion = document.getElementById("ubicacion").value;
+    let poster = document.getElementById("poster").value;
+    let fecha = document.getElementById("fecha").value;
+    let productora = document.getElementById("productora").value;
+
+    let info = {    // Crear un objeto para almacenar la información
+        nombre: nombre,
+        nombre_actor : nombreActor,
+        edad : edad,
+        ubicacion : ubicacion,
+        poster : poster,
+        fecha : fecha,
+        productora : productora
+    };
+
+    let informacionJSON = JSON.stringify(info); // Convertir el objeto a JSON
+
+    localStorage.setItem("informacion", informacionJSON); // Guardar la información en el almacenamiento local
+    alert("Información guardada correctamente."); // Opcionalmente, podrías mostrar un mensaje de éxito
 }    
 
-document.getElementById("guardar").addEventListener('click', guardar);
